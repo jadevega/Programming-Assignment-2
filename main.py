fee64a2da59e8bffa5226a56f331074a3ac48351
jadevega <116754119+jadevega@users.noreply.github.com>
2026-03-02 14:24:31 -0500

    LRU algo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from typing import List, Set, Deque
+from typing import List, Set, Deque, Dict
 from collections import deque
 
 #takes a number of requests in caches with capacity k.
@@ -25,4 +25,26 @@ def fifo(k: int, requests: List[int]) -> int:
     return num_misses
 
 
+def lru(k:int, requests: List[int]) -> int:
+    cache: Set[int] = set();
+    last_time = Dict[int, int] = {} #makes a dictionary where the key is the ID and the value is the access
 
+    num_misses = 0;
+
+    for time, req in enumerate(requests):
+        if req in cache:
+            last_time[req] = time;
+            continue
+        else:
+            num_misses+=1;
+        if(len(cache) < k):
+            cache.add(req);
+            last_time[req] = time;
+        else:
+            least = min(cache, key=lambda x: last_time[x]) #picks the least recently used.
+            cache.remove(least)
+            last_time.pop(least, None)
+            cache.add(req)
+            last_time[req] = time
+
+    return num_misses;
